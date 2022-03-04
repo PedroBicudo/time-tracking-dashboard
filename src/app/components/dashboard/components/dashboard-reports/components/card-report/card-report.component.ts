30e60a0293abcc3f2e8d36a838cebc398b76eaea
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-03-04 15:07:30 -0300

    feat(component): disable card-report hover when options button is hovered

--- src/app/components/dashboard/components/dashboard-reports/components/card-report/card-report.component.ts
+++ src/app/components/dashboard/components/dashboard-reports/components/card-report/card-report.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
 import { TimeframeType, TimeReport } from 'src/app/components/dashboard/models/time-report.model';
 import { ReportService } from './../../../../services/report/report.service';
 
@@ -15,12 +15,40 @@ export class CardReportComponent implements OnInit {
   @Input()
   timeframeOption!: { option: TimeframeType };
 
+  @ViewChild("reportContent", {static: true})
+  reportContent!: ElementRef;
+
+  @ViewChild("reportMenuOptions", {static: true})
+  reportMenuOptions!: ElementRef;
+
   constructor(
-    private reportService: ReportService
+    private reportService: ReportService,
+    private renderer: Renderer2
   ) { }
 
   ngOnInit(): void {
     this.timeframeOption = this.reportService.timeframeOption;
+
+    this.renderer.listen(this.reportContent.nativeElement, "mouseenter", () => {
+      this.renderer.addClass(this.reportContent.nativeElement, "active");
+      
+    });
+
+    this.renderer.listen(this.reportContent.nativeElement, "mouseleave", () => {
+      this.renderer.removeClass(this.reportContent.nativeElement, "active");
+
+    });
+
+    this.renderer.listen(this.reportMenuOptions.nativeElement, "mouseenter", (ev: Event) => {
+      this.renderer.removeClass(this.reportContent.nativeElement, "active");
+      
+    });
+
+    this.renderer.listen(this.reportMenuOptions.nativeElement, "mouseleave", (ev: Event) => {
+      this.renderer.addClass(this.reportContent.nativeElement, "active");
+      
+    });
+
   }
 
 }
